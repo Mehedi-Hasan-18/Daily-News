90a823d4651c745d05d8032a42f7ef283311ba9e
Mehedi-Hasan-18 <mdmehedihasanroby@gmail.com>
2025-05-28 14:52:26 +0800

    Admin register

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Article,Category,Author,ArticleImage,Ratings
 
 # Register your models here.
+admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(ArticleImage)
+admin.site.register(Ratings)
